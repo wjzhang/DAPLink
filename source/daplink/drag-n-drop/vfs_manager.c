64b31374ee736b405b58a281bf77f41c22cb244b
Z2SUTM8K\zhang.wenjin <wenjin.zhang@mesheven.com>
2019-03-11 15:17:07 +0800

    backward vfs files because NO mass storage interface.

--- source/daplink/drag-n-drop/vfs_manager.c
+++ source/daplink/drag-n-drop/vfs_manager.c
@@ -38,9 +38,6 @@
 #include "file_stream.h"
 #include "error.h"
 
-#include "target_ids.h"
-#include "target_config.h"
-
 // Set to 1 to enable debugging
 #define DEBUG_VFS_MANAGER     0
 
@@ -143,9 +140,6 @@ static uint32_t time_usb_idle;
 
 static OS_MUT sync_mutex;
 static OS_TID sync_thread = 0;
-
-// the drag&drop file is Bin/Hex format
-static bool   fileIsBinOrHex = false;
 
 // Synchronization functions
 static void sync_init(void);
@@ -326,8 +320,8 @@ void usbd_msc_read_sect(uint32_t sector, uint8_t *buf, uint32_t num_of_sectors)
         return;
     }
 
-//    // indicate msc activity
-//    main_blink_msc_led(MAIN_LED_FLASH);
+    // indicate msc activity
+    main_blink_msc_led(MAIN_LED_FLASH);
     vfs_read(sector, buf, num_of_sectors);
 }
 
@@ -348,8 +342,8 @@ void usbd_msc_write_sect(uint32_t sector, uint8_t *buf, uint32_t num_of_sectors)
         return;
     }
 
-//    // indicate msc activity
-//    main_blink_msc_led(MAIN_LED_FLASH);
+    // indicate msc activity
+    main_blink_msc_led(MAIN_LED_FLASH);
     vfs_write(sector, buf, num_of_sectors);
     if (TRASNFER_FINISHED == file_transfer_state.transfer_state) {
         return;
@@ -385,9 +379,6 @@ static bool changing_state()
 
 static void build_filesystem()
 {
-    //init 
-    fileIsBinOrHex = false;
-
     // Update anything that could have changed file system state
     file_transfer_state = default_transfer_state;
     vfs_user_build_filesystem();
@@ -429,9 +420,6 @@ static void file_change_handler(const vfs_filename_t filename, vfs_file_change_t
             // the same extension to keep track of transfer info in some cases.
             if (!(VFS_FILE_ATTR_HIDDEN & vfs_file_get_attr(new_file_data))) {
                 stream = stream_type_from_name(filename);
-                if (STREAM_TYPE_NONE != stream) {
-                        fileIsBinOrHex = true;
-				}
                 uint32_t size = vfs_file_get_size(new_file_data);
                 vfs_sector_t sector = vfs_file_get_start_sector(new_file_data);
                 transfer_update_file_info(file, sector, size, stream);
@@ -456,10 +444,7 @@ static void file_data_handler(uint32_t sector, const uint8_t *buf, uint32_t num_
 
     // this is the key for starting a file write - we dont care what file types are sent
     //  just look for something unique (NVIC table, hex, srec, etc) until root dir is updated
-    if (!file_transfer_state.stream_started && fileIsBinOrHex == true) {
-        if ( targetID == Target_UNKNOWN ) {
-            targetID = swd_init_get_target();
-        }
+    if (!file_transfer_state.stream_started) {
         // look for file types we can program
         stream = stream_start_identify((uint8_t *)buf, VFS_SECTOR_SIZE * num_of_sectors);
 
@@ -846,8 +831,6 @@ static void transfer_update_state(error_t status)
         // Set the fail reason
         fail_reason = local_status;
         vfs_mngr_printf("    Transfer finished, status: %i=%s\r\n", fail_reason, error_get_string(fail_reason));
-        target_set_state(RESET_RUN);
-        targetID = Target_UNKNOWN;
     }
 
     // If this state change is not from aborting a transfer
